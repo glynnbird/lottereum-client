35b5fbcd79f9d37b50ba2a40fd6de66bc99b1a32
danmermel <danmermel@gmail.com>
2016-04-28 13:21:58 +0100

    adding index page and moving draw page

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var app = express();
 var path = require('path');
 app.use(express.static(__dirname + '/public'));
 
-// get the public UI
+//serve the homepage
+app.get("/", function(req,res) {
+   console.log(req);
+   res.sendFile( path.join(__dirname, 'templates', 'index.html') );
+});
+
+// get a draw page
 app.get("/draw/:id", function(req,res) {
   var address = req.params.id;
-  res.sendFile( path.join(__dirname, 'public', 'index.html') );
+  res.sendFile( path.join(__dirname, 'templates', 'draw.html') );
 });
 
 app.listen(8001, '0.0.0.0', function() {
