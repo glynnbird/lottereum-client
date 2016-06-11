66bff5c8e1e3d2594e09e11065808dffdb3f9887
danielmermelstein <daniel@windermere.home>
2016-06-11 10:50:02 +0100

    get multiple values from the blockchain!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var async = require('async');
 var path = require('path');
 var lottereo = require('./lib/lottereo.js').lottereo;;
 var draw = require('./lib/draw.js');
@@ -28,12 +29,31 @@ app.get("/", function(req,res) {
 app.get("/draw/:id", function(req,res) {
   var address = req.params.id;
   var d = draw.getDraw(address);
-  d.getPot(function (eror,data) {
-      res.render ('draw', { 
-         pot: data
+
+  async.parallel([
+    d.getPot,
+    d.drawn,
+    d.numTickets,
+    d.drawDate,
+    d.winningNumber,
+    d.payout,
+    d.nextDraw,
+    d.previousDrawAddress
+  ], function(err, results) {
+     console.log(err, results);
+     res.render ('draw', { 
+         pot: results[0],
+         drawn: results[1],
+         numTickets: results[2],
+         drawDate: results[3],
+         winningNumber: results[4],
+         payout: results[5],
+         nextDraw: results[6],
+         previousDrawAddress: results[7]
      });
-     
   });
+
+
 });
 
 app.listen(8001, '0.0.0.0', function() {
